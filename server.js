7222d34a82a22259619aa8fb9c500e19a49f1053
Joe Andaverde <joe@andaverde.net>
2012-08-19 23:19:21 -0500

    Added initial support for audio playback.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.configure('production', function() {
 
 app.use(require('connect-assets')());
 
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
    res.render('index');
